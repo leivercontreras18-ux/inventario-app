b56fb903409010d26acc23853842ce0ed0318c02
leivercontreras18-ux <leivercontreras18@gmail.com>
2026-08-24 23:11:06 -0400

    Update app.py

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ else:
 st.title("📦 Control de Inventario")
 
 # Conexión a Google Sheets (CORREGIDO)
-conn = st.connection("gsheets", type=GSheetsConnection)
+conn = st.connection("gsheets", type=conn)
 df = conn.read(ttl=0)
 
 st.subheader("Estado Actual del Inventario")
